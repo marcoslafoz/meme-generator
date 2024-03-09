ec7c5b58c358ba52d1cde62033496386d3fa40ec
Marcos Lafoz <marcos.lafoz96@gmail.com>
2024-03-09 13:18:37 +0100

    feat: :heavy_plus_sign: nextui installed

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Main } from './modules/scenes/main'
 import reportWebVitals from './modules/utils/reportWebVitals';
+import { NextUIProvider } from '@nextui-org/system';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
-    <Main />
+    <NextUIProvider>
+      <Main />
+    </NextUIProvider>
   </React.StrictMode>
 );
 
--- src/modules/components/workspace/workspace.component.tsx
+++ src/modules/components/workspace/workspace.component.tsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import { Canvas } from './canvas.component'
+import { Select, SelectItem } from "@nextui-org/react"
 
 export const Workspace: React.FC = () => {
+
+  const animals = [
+    { value: 'dog', label: 'Dog' },
+    { value: 'cat', label: 'Cat' },
+    { value: 'bird', label: 'Bird' },
+    // Puedes agregar más animales aquí según sea necesario
+  ];
 
   return (
     <div className="row justify-content-center p-3">
 
       <div className="col col-auto w-75"><Canvas /></div>
-      
+      <Select
+        label="Select an animal"
+        className="max-w-xs"
+      >
+        {animals.map((animal) => (
+          <SelectItem key={animal.value} value={animal.value}>
+            {animal.label}
+          </SelectItem>
+        ))}
+      </Select>
     </div>
   )
 }

b38014c84bec4605b442c9221b011abf09ae05f0
marcoslafoz <marcos.lafoz96@gmail.com>
2024-03-11 17:46:00 +0100

    style: :art: Overflow title fixed

--- src/modules/components/header/header.component.tsx
+++ src/modules/components/header/header.component.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 export const Header: React.FC = () => {
 
   return (
-    <div className="row justify-content-center mt-2 mx-3">
-      <div className='col col-auto font-size-2 font-color-primary'><span className='text-bold'>meme</span>generator</div>
+    <div className="row justify-content-center mt-2 mx-3 header-container">
+      <div className='col col-auto font-size-2 font-color-primary text-truncate'><span className='text-bold'>meme</span>generator</div>
     </div>
   )
 }
